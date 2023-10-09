603c1cb41c3115881ab86bb505b8d2c49726b634
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-10-09 15:40:03 -0400

    Adjust card sizing

--- src/components/ValueCard.tsx
+++ src/components/ValueCard.tsx
@@ -20,13 +20,13 @@ function ValueCard<C extends React.ElementType = "div">({
   return (
     <Component
       className={twMerge(
-        "grid h-36 max-w-md grid-rows-2 justify-items-center gap-4 border border-black px-2 py-1 text-center sm:px-8 sm:py-4",
+        "flex h-48 w-80 grid-rows-2 flex-col items-center justify-center gap-4 border border-black px-2 py-1 text-center sm:px-8 sm:py-4",
         className,
       )}
       {...props}
     >
-      <h2 className="self-end text-xl font-semibold">{value.name}</h2>
-      <p className="self-start">{value.description}</p>
+      <h2 className="text-xl font-semibold">{value.name}</h2>
+      <p className="a w-64">{value.description}</p>
     </Component>
   );
 }
